2549f6a20f6d1b753cc09ab59c57a25b50fec0a6
volokhinn <csgomx@mail.ru>
2023-07-19 16:51:15 +0700

    Added animations

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import styles from '../scss/components/_modal.module.scss';
-import LongButton from './LongButton';
-import { motion } from 'framer-motion';
+import React from "react";
+import styles from "../scss/components/_modal.module.scss";
+import LongButton from "./LongButton";
+import { motion } from "framer-motion";
 
 const Modal = ({ children, setActive }) => {
   return (
     <>
-      <div className={styles.modal__wrap} onClick={() => setActive(false)}>
-        <div className={styles.modal__inner} onClick={(e) => e.stopPropagation()}>
+      <motion.div
+        initial={{ y: -1000 }}
+        animate={{ y: 0 }}
+        className={styles.modal__wrap}
+        onClick={() => setActive(false)}
+      >
+        <div
+          className={styles.modal__inner}
+          onClick={(e) => e.stopPropagation()}
+        >
           <div className={styles.top__row}></div>
-
           {children}
           <LongButton onClick={() => setActive(false)}>Продолжить</LongButton>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import styles from '../scss/index.module.scss';
-import Win from '../components/Win';
-import Settings from '../components/Settings';
-import '../scss/components/styles.css';
-import Main from '../components/Main';
-import Timer from '../components/Timer';
+import React from "react";
+import styles from "../scss/index.module.scss";
+import Win from "../components/Win";
+import Settings from "../components/Settings";
+import "../scss/components/styles.css";
+import Main from "../components/Main";
+import Timer from "../components/Timer";
 
-import LongButton from '../components/LongButton';
-import ShortButton from '../components/ShortButton';
+import LongButton from "../components/LongButton";
+import ShortButton from "../components/ShortButton";
 
-import settings from '../assets/svg/settings.svg';
-import play from '../assets/svg/play.svg';
-import stop from '../assets/svg/stop.svg';
-import Break from '../components/Break,';
-import Modal from '../components/Modal';
+import settings from "../assets/svg/settings.svg";
+import play from "../assets/svg/play.svg";
+import stop from "../assets/svg/stop.svg";
+import Break from "../components/Break,";
+import Modal from "../components/Modal";
 
-import { selectTimerData, setTimer } from '../redux/slices/timerSlice';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { selectTimerData, setTimer } from "../redux/slices/timerSlice";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Home = () => {
   const [winActive, setWinActive] = React.useState(true);
@@ -31,69 +32,99 @@ const Home = () => {
     dispatch(setTimer(isStop));
   };
 
-  const { background, defaultTime: time, stateTimer } = useSelector(selectTimerData);
+  const {
+    background,
+    defaultTime: time,
+    stateTimer,
+  } = useSelector(selectTimerData);
 
   return (
     <>
       <img className={styles.bg__img} src={background} alt={background} />
       <div className={styles.content}>
-        <div className={styles.content__left}>
-          {stateTimer === 'unActive' ? (
-            <>
-              <Main />
-              <div className={styles.buttons}>
-                {settingsActive ? (
-                  <LongButton
-                    onClick={() => onClickTimer()}
-                    disabled={true}
-                    style={{ opacity: '0.5' }}>
-                    Начать <img className={styles.btn__icon} src={play} alt={play} />
-                  </LongButton>
-                ) : (
+        <AnimatePresence>
+          <div className={styles.content__left}>
+            {stateTimer === "unActive" ? (
+              <motion.div
+                initial={{ y: -1000 }}
+                animate={{ y: 0 }}
+                transition={{ duration: 0.4 }}
+                key="motionMain"
+              >
+                <Main />
+                <div className={styles.buttons}>
+                  {settingsActive ? (
+                    <LongButton
+                      onClick={() => onClickTimer()}
+                      disabled={true}
+                      style={{ opacity: "0.5" }}
+                    >
+                      Начать{" "}
+                      <img className={styles.btn__icon} src={play} alt={play} />
+                    </LongButton>
+                  ) : (
+                    <LongButton onClick={() => onClickTimer()}>
+                      Начать{" "}
+                      <img className={styles.btn__icon} src={play} alt={play} />
+                    </LongButton>
+                  )}
+                  <ShortButton onClick={() => setSettingsActive(true)}>
+                    <img
+                      className={styles.btn__icon}
+                      src={settings}
+                      alt={settings}
+                    />
+                  </ShortButton>
+                </div>
+                <p className={styles.description}>
+                  Чтобы начать сессию {Math.floor(defaultTime / 60)} минут
+                  минут, жми <span style={{ color: "#fff" }}>Начать</span>
+                </p>
+              </motion.div>
+            ) : stateTimer === "active" ? (
+              <motion.div
+                initial={{ y: -1000 }}
+                animate={{ y: 0 }}
+                transition={{ duration: 0.4 }}
+                key="motionTimer"
+              >
+                <Timer />
+                <div className={styles.buttons}>
                   <LongButton onClick={() => onClickTimer()}>
-                    Начать <img className={styles.btn__icon} src={play} alt={play} />
+                    Стоп{" "}
+                    <img className={styles.btn__icon} src={stop} alt={stop} />
                   </LongButton>
+                </div>
+
+                <p className={styles.description}>
+                  Жми <span style={{ color: "#fff" }}>Стоп</span> если решил
+                  закончить раньше
+                </p>
+              </motion.div>
+            ) : (
+              <motion.div
+                initial={{ y: -1000 }}
+                animate={{ y: 0 }}
+                transition={{ duration: 0.4 }}
+                key="motionBreak"
+              >
+                <Break />
+                <div className={styles.buttons}>
+                  <LongButton onClick={() => onClickTimer()}>
+                    Начать{" "}
+                    <img className={styles.btn__icon} src={play} alt={play} />
+                  </LongButton>
+                </div>
+
+                {winActive && (
+                  <Modal active={winActive} setActive={setWinActive}>
+                    <Win />
+                  </Modal>
                 )}
-                <ShortButton onClick={() => setSettingsActive(true)}>
-                  <img className={styles.btn__icon} src={settings} alt={settings} />
-                </ShortButton>
-              </div>
-
-              <p className={styles.description}>
-                Чтобы начать сессию {Math.floor(defaultTime / 60)} минут минут, жми{' '}
-                <span style={{ color: '#fff' }}>Начать</span>
-              </p>
-            </>
-          ) : stateTimer === 'active' ? (
-            <>
-              <Timer />
-              <div className={styles.buttons}>
-                <LongButton onClick={() => onClickTimer()}>
-                  Стоп <img className={styles.btn__icon} src={stop} alt={stop} />
-                </LongButton>
-              </div>
-
-              <p className={styles.description}>
-                Жми <span style={{ color: '#fff' }}>Стоп</span> если решил закончить раньше
-              </p>
-            </>
-          ) : (
-            <>
-              <Break />
-              <div className={styles.buttons}>
-                <LongButton onClick={() => onClickTimer()}>
-                  Начать <img className={styles.btn__icon} src={play} alt={play} />
-                </LongButton>
-              </div>
-
-              {winActive && (
-                <Modal active={winActive} setActive={setWinActive}>
-                  <Win />
-                </Modal>
-              )}
-            </>
-          )}
-        </div>
+              </motion.div>
+            )}
+          </div>
+        </AnimatePresence>
         {settingsActive && (
           <Modal active={settingsActive} setActive={setSettingsActive}>
             <>
@@ -102,14 +133,14 @@ const Home = () => {
           </Modal>
         )}
       </div>
-      {stateTimer === 'active' && defaultTime === 1500 ? (
+      {stateTimer === "active" && defaultTime === 1500 ? (
         <div className={`${styles.timer__row} ${styles.timer25min}`}></div>
-      ) : stateTimer === 'active' && defaultTime === 2700 ? (
+      ) : stateTimer === "active" && defaultTime === 2700 ? (
         <div className={`${styles.timer__row} ${styles.timer45min}`}></div>
-      ) : stateTimer === 'active' && defaultTime === 3600 ? (
+      ) : stateTimer === "active" && defaultTime === 3600 ? (
         <div className={`${styles.timer__row} ${styles.timer60min}`}></div>
       ) : (
-        ''
+        ""
       )}
     </>
   );
